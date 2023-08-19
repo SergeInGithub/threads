d6d270e9166d7c0ded3f9742626ef04d13e2e6db
SergeInGithub <nyiriserge@gmail.com>
2023-08-19 17:48:12 +0200

    ft(userModel): add user model & user action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,1 +1,32 @@
 'user server';
+
+import { revalidatePath } from 'next/cache';
+import User from '../models/user.model';
+import { connectTodDB } from '../mongoose';
+
+export async function updateUser(
+  userId: string,
+  username: string,
+  name: string,
+  bio: string,
+  image: string,
+  path: string,
+): Promise<void> {
+  connectTodDB();
+
+  try {
+    await User.findOneAndUpdate(
+      { id: userId },
+      { username: username.toLowerCase(), name, bio, image, onboarded: true },
+      {
+        upsert: true,
+      },
+    );
+
+    if (path === '/profile/edit') {
+      revalidatePath(path);
+    }
+  } catch (error: any) {
+    throw new Error(`Failed to create/update user: ${error.message}`);
+  }
+}
