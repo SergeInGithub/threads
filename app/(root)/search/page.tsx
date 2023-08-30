18a194b61fa70d82f269dcea92c1afcf8190858a
SergeInGithub <nyiriserge@gmail.com>
2023-08-30 20:45:26 +0200

    ft(search): add search page

---
+++ app/(root)/search/page.tsx
@@ -0,0 +1,54 @@
+import PostThread from '@/components/forms/PostThread';
+import ProfileHeader from '@/components/shared/ProfileHeader';
+import { fetchUser, fetchUsers } from '@/lib/actions/user.actions';
+import { currentUser } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+import { profileTabs } from '@/constants';
+import Image from 'next/image';
+import ThreadsTab from '@/components/shared/ThreadsTab';
+import UserCard from '@/components/cards/UserCard';
+
+async function Page() {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) redirect('/onboarding');
+
+  //* Fetch users
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: '',
+    pageNumber: 1,
+    pageSize: 25,
+  });
+
+  return (
+    <section>
+      <h1 className="mb-10 head-text">Search</h1>
+
+      <div className="flex flex-col mt-14 gap-9">
+        {result.users.length === 0 ? (
+          <p className="no-result"> No users</p>
+        ) : (
+          <>
+            {result.users.map((person) => (
+              <UserCard
+                key={person.id}
+                id={person.id}
+                name={person.name}
+                username={person.username}
+                imgUrl={person.image}
+                personType="User"
+              />
+            ))}
+          </>
+        )}
+      </div>
+    </section>
+  );
+}
+
+export default Page;
