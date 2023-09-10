ae0f878ba0cb84409f13c463c187562a9b957c8c
SergeInGithub <nyiriserge@gmail.com>
2023-09-10 19:48:31 +0200

    ft(Comment): fix comment feature

--- components/forms/Comment.tsx
+++ components/forms/Comment.tsx
@@ -1,37 +1,35 @@
 'use client';
 
+import { z } from 'zod';
+import Image from 'next/image';
 import { useForm } from 'react-hook-form';
+import { usePathname } from 'next/navigation';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Button } from '@/components/ui/button';
+
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
-  FormMessage,
 } from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
-import * as z from 'zod';
-import { usePathname, useRouter } from 'next/navigation';
-// import { updateUser } from '@/lib/actions/user.actions';
+
+import { Input } from '../ui/input';
+import { Button } from '../ui/button';
+
 import { CommentValidation } from '@/lib/validations/thread';
-import Image from 'next/image';
 import { addCommentToThread } from '@/lib/actions/thread.actions';
-// import { createThread } from '@/lib/actions/thread.actions';
 
 interface Props {
   threadId: string;
-  currentUserId: string;
   currentUserImg: string;
+  currentUserId: string;
 }
 
 function Comment({ threadId, currentUserImg, currentUserId }: Props) {
-  const router = useRouter();
   const pathname = usePathname();
 
-  const form = useForm({
+  const form = useForm<z.infer<typeof CommentValidation>>({
     resolver: zodResolver(CommentValidation),
     defaultValues: {
       thread: '',
@@ -48,28 +46,30 @@ function Comment({ threadId, currentUserImg, currentUserId }: Props) {
 
     form.reset();
   };
+
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="comment-form">
+      <form className="comment-form" onSubmit={form.handleSubmit(onSubmit)}>
         <FormField
           control={form.control}
           name="thread"
           render={({ field }) => (
-            <FormItem className="flex items-center w-full gap-3">
+            <FormItem className="flex w-full items-center gap-3">
               <FormLabel>
                 <Image
                   src={currentUserImg}
-                  alt="profile image"
+                  alt="current_user"
                   width={48}
                   height={48}
-                  className="object-cover rounded-full"
+                  className="rounded-full object-cover"
                 />
               </FormLabel>
-              <FormControl className="bg-transparent border-none">
+              <FormControl className="border-none bg-transparent">
                 <Input
                   type="text"
+                  {...field}
                   placeholder="Comment..."
-                  className="outline-none no-focus text-light-1"
+                  className="no-focus text-light-1 outline-none"
                 />
               </FormControl>
             </FormItem>
