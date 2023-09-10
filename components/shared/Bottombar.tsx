02fd6d25a7ff238bf9ce32660666e15f7d34e162
SergeInGithub <nyiriserge@gmail.com>
2023-09-10 19:47:52 +0200

    ft(Bottombar): fix bottombar

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -25,8 +25,9 @@ function Bottombar() {
               <Image
                 src={link.imgURL}
                 alt={link.label}
-                width={24}
-                height={24}
+                width={16}
+                height={16}
+                className="object-contain"
               />
 
               <p className="text-subtle-medium text-light-1 max-sm:hidden">
